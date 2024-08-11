e7bc119cfc8d37ff9687056ee1ae53e7c98386ae
ldelcourt <lou.delcourt@insa-lyon.fr>
2024-08-11 12:19:15 +0200

    Initial commit

---
+++ nvim/lua/plugins/nvim-tree-sitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    ---@type TSConfig
+    opts = {
+      ensure_installed = {
+        "bash",
+        "dart",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "svelte",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
